b78ca716a295a3ddfb9331be640ef5ebe4747ca3
Brent Biddy <bbiddy@wustl.edu>
2019-02-04 16:52:12 -0600

    Added scratch.pad.R which contains commands for testing.

---
+++ R/scratch.pad.R
@@ -0,0 +1,59 @@
+
+
+library(tidyverse)
+
+library(SingleCellExperiment)
+
+source(file = "dataTools.R")
+
+monoclePath <- "../../../Desktop/unsupervised timeline all data.RDS"
+
+seuratPath <- "../../../Desktop/Warner_CCA_after_TSNE.RDS"
+
+monocleCDS <- readRDS(monoclePath)
+
+seuratObj <- readRDS(seuratPath)
+
+
+dataList <- list(seurat = seuratObj,
+                monocle = monocleCDS)
+
+
+sce <- makeVizData(dataSets = dataList)
+
+
+
+
+
+plotData <- as_tibble(reducedDim(sce, "cca.aligned.Seurat"))
+
+plotData <- reducedDim(sce, "cca.aligned.Seurat")
+
+cellBCs <- rownames(plotData)
+
+plotData <- as_tibble(plotData)
+
+plotData$CellBCs <- cellBCs
+
+
+metaData <- colData(sce)
+
+metaData$CellBCs <- rownames(metaData)
+
+
+metaData <- as_tibble(metaData)
+
+
+
+ggplot(data = plotData) + geom_point(aes(x = ACC1, y = ACC2))
+
+
+generatePlotData <- function(sce, metaData, geneChoice, redMethod){
+
+  plotData <- as_tibble(reducedDim(sce, redMethod))
+
+  plotData[, colnames(sce@colData)] <- sce@colData[rownames(plotData), colnames(sce@colData)]
+
+}
+
+ggplot(data = allData) + geom_point(aes(x = ACC1, y = ACC2, color = State.Monocle))
