b643a7976b00f25a1a9b0fe94edefc1efeaf2af2
Brent Biddy <bbiddy@wustl.edu>
2019-02-05 12:00:29 -0600

    Added sceTools.R which contains functions to manipulate sce objects and generate ggplot compatible data sets for visualization.

--- R/sceTools.R
+++ R/sceTools.R
@@ -1,26 +1,42 @@
 
 
-getEmbeddings <- function(sce, redMethod, cells = NULL){
-  
-  embeddings <- as(reducedDim(sce, redMethod), rownames = NA)
-  
+getEmbeddings <- function(sce, redMethod, cells = FALSE){
+
+  embeddings <- reducedDim(sce, redMethod)
+
   if(cells){
-    
+
     embeddings <- embeddings[cells, ]
   }
-  
+
   return(embeddings)
 }
 
-getMetaData <- function(sce, varName, cells = NULL){
-  
+getMetaData <- function(sce, varName, cells = FALSE){
+
   metaData <- colData(sce)[varName]
-  
-  metaData <- met
-  
+
   if(cells){
-    
+
     metaData <- metaData[cells, ]
   }
-  
+
+  return(metaData)
+
+}
+
+makePlotData <- function(sce, redMethod, metaVar, cells = FALSE){
+
+  embeddings <- getEmbeddings(sce = sce, redMethod = redMethod, cells = cells)
+
+  embeddings <- as(embeddings[,1:2], "DataFrame")
+
+  metaData <- getMetaData(sce = sce, varName = metaVar, cells = cells)
+
+  plotData <- merge(embeddings, metaData, by = "row.names")
+
+  plotData <- as.data.frame(plotData)
+
+  return(plotData)
+
 }
