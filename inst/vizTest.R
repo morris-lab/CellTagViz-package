b963bff315cbf49677538653c7ebf1747d112ddc
Brent Biddy <bbiddy@wustl.edu>
2019-02-22 10:15:29 -0600

    Fixed file paths for monocle and seurat objects

--- inst/vizTest.R
+++ inst/vizTest.R
@@ -8,9 +8,14 @@ library(Seurat)
 
 library(CellTagViz)
 
-monoclePath <- "../../../Desktop/unsupervised timeline all data.RDS"
 
-seuratPath <- "../../../Desktop/Warner_CCA_after_TSNE.RDS"
+monoclePath <- "~/../Desktop/unsupervised timeline all data.RDS"
+
+#monoclePath <- "../../../Desktop/unsupervised timeline all data.RDS"
+
+#seuratPath <- "../../../Desktop/Warner_CCA_after_TSNE.RDS"
+
+seuratPath <- "~/../Desktop/Warner_CCA_after_TSNE.RDS"
 
 monocleCDS <- readRDS(monoclePath)
 
